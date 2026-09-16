43715eab51c86d9321a3ee745a45c4dd93bcd9cf
Nshrey <nshrey94@gmail.com>
2026-09-16 13:16:28 +0530

    Allow Vercel Front End

--- server/server.js
+++ server/server.js
@@ -6,11 +6,13 @@ const app = express();
 const server = createServer(app);
 
 const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:5173",
-  },
-});
-
+    cors: {
+      origin: [
+        "http://localhost:5173",
+        "https://quiz-buzzer-steel.vercel.app",
+      ],
+    },
+  });
 let winner = null;
 let adminSocketId = null;
 
