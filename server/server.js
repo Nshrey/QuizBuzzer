d661d68667d5b61b6a0a2bc11dbc4431b51a103a
Nshrey <nshrey94@gmail.com>
2026-09-16 12:54:52 +0530

    Prepare server for deployment

--- server/server.js
+++ server/server.js
@@ -85,6 +85,8 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("Buzzer server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Buzzer server running on port ${PORT}`);
 });
